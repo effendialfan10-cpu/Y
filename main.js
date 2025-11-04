63670de7d200360b10019a5711d99d005261e33b
effendialfan10-cpu <effendialfan10@gmail.com>
2025-11-04 19:34:46 +0700

    java

---
+++ main.js
@@ -0,0 +1,6 @@
+smart-health/
+│
+├── index.html          ← halaman utama
+├── style.css           ← file tampilan (desain & animasi)
+├── script.js           ← logika (perhitungan, grafik, dark mode)
+└── chart.js            ← pustaka grafik (Chart.js dari CDN)
